85a6927521fad6ffca940497d46736bafe5fa669
atticus <atticus.pak@gmail.com>
2024-03-11 09:49:48 -0700

    Added main menu still need to fix some stuff

--- Assets/Scripts/Server/Lobby.cs
+++ Assets/Scripts/Server/Lobby.cs
@@ -12,10 +12,41 @@ namespace Assets.Scripts.Server
             
         }
 
+
         public void AddPlayer(SInternalPlayer player)
         {
-            
+            if (team1.Count > team2.Count)
+            {
+                team2.Add(player);
+            }
+            else
+            {
+                team1.Add(player);
+            }
+            playersInLobby.Add(player);
+        }
+
+        public void RemovePlayers(SInternalPlayer player)
+        {
+            if (team1.Contains(player))
+            {
+                team1.Remove(player);
+            }
+            else
+            {
+                team2.Remove(player);
+            }
+            playersInLobby.Remove(player);
         }
         
+        public IReadOnlyList<SInternalPlayer> Team1()
+        {
+            return team1;
+        }
+        
+        public IReadOnlyList<SInternalPlayer> Team2()
+        {
+            return team2;
+        }
     }
 }
