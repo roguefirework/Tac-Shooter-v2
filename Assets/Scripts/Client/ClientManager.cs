cbbfe3b0192576fa26ec63c247ece896b870b27f
atticus <atticus.pak@gmail.com>
2024-03-19 10:19:16 -0700

    Partil Client Lobby

--- Assets/Scripts/Client/ClientManager.cs
+++ Assets/Scripts/Client/ClientManager.cs
@@ -1,18 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Riptide;
+using Shared;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClientManager : MonoBehaviour
 {
+    public static ClientManager Instance { get; private set; };
+
+    [SerializeField] private string mainMenuScene;
+    [SerializeField] private ushort port;
+    
+    
+    private PersistentData _data;
+    private Client _client;
+    
     // Start is called before the first frame update
     void Start()
     {
+        Instance = this;
+        _data = PersistentData.Instance;
+        _client = new Client();
+        _client.ConnectionFailed += Disconnected;
+        _client.Disconnected += (_, _) => Disconnected(null,null);
+        _client.Connected += Connected;
+        bool connected = _client.Connect(_data.TargetIP + $":{port}",useMessageHandlers:false);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Connected(object sender, EventArgs e)
+    {
+        _client.Send(Message.Create(MessageSendMode.Reliable,ClientToServerProtocol.JoinGame).AddString(_data.Username));
+        SceneManager.LoadScene(mainMenuScene,LoadSceneMode.Additive);
+    }
+
+    public void FixedUpdate()
+    {
+        _client.Update();
+    }
+
+    private void Disconnected(object sender, ConnectionFailedEventArgs connectionFailedEventArgs)
     {
         
+        SceneManager.LoadScene("Main Menu",LoadSceneMode.Single);
+    }
+
+    public void OnApplicationQuit()
+    {
+        _client.Disconnect();
     }
 }
